1f6202fdeadc6f86ea0c41ed33930761a7685030
Vasilyev Maksim <3103maksim96@gmail.com>
2023-10-26 13:29:54 +0600

    Включил интернет лол

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let i = 1;
+// const radio = document.getElementsByName("r"); //masive[r1, r2 ...]
+
+// function checkedSelectRadio() {
+//   radio.forEach((element) => {
+//     if (element.checked == true) {
+//       if (i - 1 != element.id.split("")[1]) {
+//         i = element.id.split("")[1];
+//       }
+//     }
+//   });
+// }
+setInterval(() => {
+  if (i === 6) {
+    i = 1;
+  }
+  // checkedSelectRadio();
+  document.getElementById(`r${i}`).checked = true;
+  i++;
+}, 2300);
+
+const items = document.getElementsByTagName("items");
+console.log(items);
